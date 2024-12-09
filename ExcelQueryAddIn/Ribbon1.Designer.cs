8df90b088835c5368c302c95f191b5af032a467b
glhol <glhol@DESKTOP-AA0A11Q>
2024-12-08 21:00:08 -0500

    Add project files.

---
+++ ExcelQueryAddIn/Ribbon1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ExcelQueryAddIn
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Office.Tools.Ribbon.RibbonDialogLauncher ribbonDialogLauncherImpl1 = this.Factory.CreateRibbonDialogLauncher();
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.comboBox1 = this.Factory.CreateRibbonComboBox();
+            this.box1 = this.Factory.CreateRibbonBox();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.paramGroup = this.Factory.CreateRibbonGroup();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.paramGroup);
+            this.tab1.Label = "Query";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.DialogLauncher = ribbonDialogLauncherImpl1;
+            this.group1.Items.Add(this.comboBox1);
+            this.group1.Items.Add(this.box1);
+            this.group1.Items.Add(this.button1);
+            this.group1.Label = "Query selection";
+            this.group1.Name = "group1";
+            this.group1.DialogLauncherClick += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.group1_DialogLauncherClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Label = "Query";
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SizeString = "Select a query from the list";
+            this.comboBox1.Text = null;
+            // 
+            // box1
+            // 
+            this.box1.Name = "box1";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::ExcelQueryAddIn.Properties.Resources.AvengerLogo_Outline3;
+            this.button1.Label = "Execute Query";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // paramGroup
+            // 
+            this.paramGroup.Label = "Parameters";
+            this.paramGroup.Name = "paramGroup";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonComboBox comboBox1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonBox box1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup paramGroup;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ ExcelQueryAddIn/Ribbon1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ExcelQueryAddIn
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            // add RibbonDropDownItem to comboBox1
+            RibbonDropDownItem item1 = Globals.Factory.GetRibbonFactory().CreateRibbonDropDownItem();
+            item1.Label = "Option 1 - This is a long option";
+            comboBox1.Items.Add(item1);
+
+            RibbonDropDownItem item2 = Globals.Factory.GetRibbonFactory().CreateRibbonDropDownItem();
+            item2.Label = "Option 2";
+            comboBox1.Items.Add(item2);
+
+            RibbonDropDownItem item3 = Globals.Factory.GetRibbonFactory().CreateRibbonDropDownItem();
+            item3.Label = "Option 3";
+            comboBox1.Items.Add(item3);
+            //button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            //comboBox1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            
+            
+            
+
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            //MessageBox.Show("Button clicked.");
+            // get the selected item from comboBox1
+
+            //paramGroup.Items.Add(Globals.Factory.GetRibbonFactory().CreateRibbonEditBox());
+
+
+            var selection = comboBox1.Text;
+            MessageBox.Show(selection);
+        }
+
+        private void group1_DialogLauncherClick(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Dialog Launcher clicked.");
+        }
+    }
+}
